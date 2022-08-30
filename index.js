aaffa3c081aa69138a39b732cbd35cedd8738909
Carolina Hernandez <carolinahernandez@carolinas-mbp-2.myfiosgateway.com>
2022-08-30 19:18:09 -0400

    done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let content = document.querySelector('.js-generated.content')
 
 let header = document.createElement('h1')
 header.setAttribute('class', 'dog-name')
-header.append('Rizzo')
+header.textContent = ('Rizzo')
 content.append(header)
 
 let dogContent = document.createElement('div')
@@ -13,10 +13,32 @@ content.append(dogContent)
 let dogImage = document.createElement('img')
 dogImage.setAttribute('class', 'dog-image')
 dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+dogContent.append(dogImage)
 
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+dogContent.append(dogDetails)
 
 
+let headerThree = document.createElement('h3')
+headerThree.textContent = ('Description:')
+dogDetails.append(headerThree)
+
+let paragraph = document.createElement('p')
+paragraph.textContent = ('This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.')
+dogDetails.append(paragraph)
+
+let headerfour = document.createElement('h3')
+headerfour.textContent = ('Feeding Times:')
+dogDetails.append(headerfour)
+
+let ul = document.createElement('ul')
+
+let feedingTimes = ['9:30 am', '12:00pm', '5:00pm']
+for (let i=0; i < feedingTimes.length; i++){
+    let li = document.createElement('li');
+    li.textContent = feedingTimes[i]
+    ul.append(li)
+}
+
+dogDetails.append(ul)
